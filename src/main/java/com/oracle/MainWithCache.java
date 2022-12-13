16cd6ddb1fb1de377fcfcf12169f41c2964a1f01
Kanwal <akanwal@informatica.com>
2022-12-13 08:48:58 +0530

    Updated

--- src/main/java/com/oracle/MainWithCache.java
+++ src/main/java/com/oracle/MainWithCache.java
@@ -22,12 +22,12 @@ public class MainWithCache {
 //        allTales = awsdbManager.getTables(dbName);
 //        System.out.printf("Size = %s, allTables = %s%n", allTales.size(), allTales);
         CacheManager cacheManager = new CacheManager();
-        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(dbName)));
+        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(1, dbName)));
 
         // Using cachel
-        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(dbName)));
-        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(dbName)));
-        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(dbName)));
+        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(2, dbName)));
+        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(3, dbName)));
+        System.out.println(cacheManager.generateOrgMonitoringResponse(new DBDetail(4, dbName)));
 //        oracleDBManager.print("pam12cr2", "ORDERS");
     }
 
